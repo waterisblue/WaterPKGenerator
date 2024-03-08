a44662a240410d41b9848ebe103fe5a7bfba50ee
waterIB <1772756410@qq.com>
2024-03-08 15:08:39 +0800

    Start方法增加了一下逻辑

--- pk/pk_manager.go
+++ pk/pk_manager.go
@@ -1,6 +1,7 @@
 package pk
 
 import (
+	"fmt"
 	"pkgenerate/config"
 	sqlmanage "pkgenerate/sql_manage"
 	"strconv"
@@ -14,14 +15,27 @@ type PKPrefixManager struct {
 var PKManager map[string]PKPrefixManager
 
 // 开始生成key
-func Start() {
+func Init() {
 	// 获取配置参数内容
 	configMap := config.GetConfig()
 	cacheNum, _ := strconv.Atoi(configMap["pk.cache.num"])
 	// 查询前缀总数
 	prefixCount, _ := sqlmanage.SelectPrefixCount()
 
-	for i := 0; i < prefixCount; i++ {
+	PKManager = make(map[string]PKPrefixManager)
 
+	for i := 1; i <= prefixCount; i++ {
+		prefixName, prefixEndPK, err := sqlmanage.SelectPrefixById(i)
+		if err != nil {
+			fmt.Println("前缀名：" + prefixName + "生成错误，错误原因：" + err.Error())
+			err = nil
+			continue
+		}
+
+		// 初始化生成主键
+		PKManager[prefixName] = PKPrefixManager{
+			pks:      make(chan string, cacheNum),
+			isActive: true,
+		}
 	}
 }
